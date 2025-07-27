ea3fdee553e13234de0513483ea0406217b7158f
iankj1 <130313154+iankj1@users.noreply.github.com>
2025-07-27 15:54:25 +0300

    Update urls.py

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('chats.urls')),
+    path('api/', include('messaging_app.chats.urls')),  # API routes for conversations and messages
+    path('api-auth/', include('rest_framework.urls')),  # Required for checker and login via browsable API
 ]
