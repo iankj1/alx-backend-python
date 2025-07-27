ef228d18c5af5c57ba73f1e024d950e21f7feafb
iankj1 <130313154+iankj1@users.noreply.github.com>
2025-07-27 18:23:35 +0300

    Update settings.py

--- Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/settings.py
@@ -57,11 +57,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'chats.middleware.RequestLoggingMiddleware',
-    'chats.middleware.OffensiveLanguageMiddleware',  
-    'chats.middleware.RequestLoggingMiddleware', 
-]
 
+    # Add your custom middlewares below
+    'chats.middleware.RequestLoggingMiddleware',
+    'chats.middleware.RestrictAccessByTimeMiddleware',
+    'chats.middleware.OffensiveLanguageMiddleware',
+]
 
 ROOT_URLCONF = 'messaging_app.urls'
 
