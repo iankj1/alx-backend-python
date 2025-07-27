bed0cdb99c1e1c7c4d525e50f613b517bf66e58e
iankj1 <130313154+iankj1@users.noreply.github.com>
2025-07-27 16:12:03 +0300

    Update permissions.py

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,15 +1,25 @@
 from rest_framework import permissions
 
-class IsOwnerOrParticipant(permissions.BasePermission):
+class IsParticipantOfConversation(permissions.BasePermission):
     """
-    Custom permission to allow only conversation participants to view messages.
+    Custom permission to:
+    - Allow only authenticated users
+    - Allow only participants in a conversation to view, send, update or delete messages
     """
 
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
-        # For messages
+        # If object is a Message
         if hasattr(obj, 'sender'):
-            return obj.sender == request.user or request.user in obj.conversation.participants.all()
-        # For conversations
+            return (
+                obj.sender == request.user or
+                request.user in obj.conversation.participants.all()
+            )
+
+        # If object is a Conversation
         if hasattr(obj, 'participants'):
             return request.user in obj.participants.all()
+
         return False
