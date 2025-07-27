5e82e579ac040203b622baa514ef18db245ea6e2
iankj1 <130313154+iankj1@users.noreply.github.com>
2025-07-27 15:49:09 +0300

    Update views.py

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -6,3 +6,29 @@ from django.http import JsonResponse
 
 def index(request):
     return JsonResponse({"message": "Messaging App API is live!"})
+# messaging_app/chats/views.py
+
+from rest_framework import viewsets, status, filters
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['participants__username']
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
