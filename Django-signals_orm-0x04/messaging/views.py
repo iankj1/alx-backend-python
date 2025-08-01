28e6a8734cbe9f524480d4baf96ad516e5651c78
iankj1 <130313154+iankj1@users.noreply.github.com>
2025-08-01 16:39:15 +0300

    Update views.py

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,21 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Message
-from django.db.models import Prefetch
 
 @login_required
-def threaded_messages_view(request):
-    # Filter top-level messages involving current user
-    top_messages = Message.objects.filter(
-        parent_message__isnull=True
-    ).filter(
-        sender=request.user
-    ).select_related('sender', 'receiver') \
-     .prefetch_related(
-         Prefetch('replies', queryset=Message.objects.select_related('sender', 'receiver').order_by('timestamp'))
-     ).order_by('-timestamp')
+def unread_messages_view(request):
+    unread_msgs = Message.unread.for_user(request.user)
 
-    context = {
-        'messages': top_messages
-    }
-    return render(request, 'messaging/threaded_messages.html', context)
+    return render(request, 'messaging/unread_messages.html', {
+        'unread_messages': unread_msgs
+    })
