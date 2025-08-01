eda6bcc319092e92262214e377a3ec708c2fcabb
iankj1 <130313154+iankj1@users.noreply.github.com>
2025-08-01 16:34:13 +0300

    Update views.py

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,10 +1,21 @@
-from django.shortcuts import redirect
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
+from .models import Message
+from django.db.models import Prefetch
 
 @login_required
-def delete_user(request):
-    if request.method == 'POST':
-        user = request.user
-        user.delete()
-        return redirect('account_deleted')  # or redirect to homepage or logout
+def threaded_messages_view(request):
+    # Filter top-level messages involving current user
+    top_messages = Message.objects.filter(
+        parent_message__isnull=True
+    ).filter(
+        sender=request.user
+    ).select_related('sender', 'receiver') \
+     .prefetch_related(
+         Prefetch('replies', queryset=Message.objects.select_related('sender', 'receiver').order_by('timestamp'))
+     ).order_by('-timestamp')
+
+    context = {
+        'messages': top_messages
+    }
+    return render(request, 'messaging/threaded_messages.html', context)
